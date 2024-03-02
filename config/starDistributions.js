f23357732bbf472c43c6845677e7976dc09aea4f
Jawad Ahmed <mail.jawadahmed@gmail.com>
2024-03-02 01:35:31 -0800

    first commit

---
+++ config/starDistributions.js
@@ -0,0 +1,5 @@
+export const starTypes = {
+    percentage : [76.45, 12.1, 7.6, 3.0, 0.6, 0.13],
+    color: [0x0082ff, 0x0082ff, 0x0082ff, 0x0082ff, 0xcad7ff, 0x0082ff],
+    size: [0.7, 0.7, 1.15, 1.48, 2.0, 2.5, 3.5]
+}
